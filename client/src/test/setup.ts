e5d765a7a61c4557be3eb33a97d1906abec9b873
isaquemed <isaquesiqueiraamaral@gmail.com>
2026-01-26 20:06:54 -0300

    atualização

--- client/src/test/setup.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// Cleanup após cada teste
-afterEach(() => {
-  cleanup();
-});
